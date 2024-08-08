bf1635b6774e06c0220256d4fa7e69cfe5f67a5f
HikkeyTheWeirdo <congnguyen051906@gmail.com>
2024-08-08 14:14:53 +0200

    Created class MovieItem in Items to specify all the data needed to scrape from each movie.
    Installed ipython to use scrapy shell commands to
    test the commands to make sure getting the right data.
    
    Edited in "scrapy.cfg" file in settings section to allow
    shell = ipython

--- imdb_scrape/imdb_scrape/items.py
+++ imdb_scrape/imdb_scrape/items.py
@@ -10,3 +10,21 @@ class ImdbScrapeItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class MovieItem(scrapy.Item):
+   url = scrapy.Field()
+   title = scrapy.Field()
+   year = scrapy.Field()
+   parrent_guide = scrapy.Field()
+   run_time = scrapy.Field()
+   rating = scrapy.Field()
+   num_reviews = scrapy.Field()
+   popularity = scrapy.Field()
+   genre = scrapy.Field()
+   description = scrapy.Field()
+   director = scrapy.Field()
+   writers = scrapy.Field()
+   stars = scrapy.Field()
+   budget = scrapy.Field()
+   gross_na = scrapy.Field()
+   gross_globe = scrapy.Field()
--- imdb_scrape/imdb_scrape/spiders/imdb_spider.py
+++ imdb_scrape/imdb_scrape/spiders/imdb_spider.py
@@ -1,10 +1,12 @@
 import scrapy
+from imdb_scrape.items import MovieItem
+
 
 
 class ImdbSpiderSpider(scrapy.Spider):
     name = "imdb_spider"
-    allowed_domains = ["imdb.com"]
-    start_urls = ["https://imdb.com"]
+    allowed_domains = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
+    start_urls = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
 
     def parse(self, response):
         pass
